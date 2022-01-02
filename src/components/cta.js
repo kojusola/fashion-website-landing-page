2c47f0d8867664447297c46522ec65eda7d1ac55
Fafemi Adeola <adeola5678@gmail.com>
2022-01-02 09:10:33 +0100

    animations

--- src/components/cta.js
+++ src/components/cta.js
@@ -5,82 +5,84 @@ import ArrowIcon from "../images/collections-arrow.svg"
 import CtaSvg from "../images/cta-svg.svg"
 // import { Link } from "gatsby"
 
-const Cta = ({ siteTitle }) => (
-  <Chakra.Box position="relative" mb="-28px" width="100%">
-    <Chakra.Image width="100%" src={CtaImage}></Chakra.Image>
-    <Chakra.Box width="100%" position="absolute" top="0px" bottom="0px">
-      <Chakra.Box
-        display="flex"
-        alignItems="center"
-        px="60px"
-        maxWidth="1394px"
-        mx="auto"
-      >
-        <Chakra.Box paddingTop="56px" alignItems="center" display="flex">
-          <Chakra.Text
-            marginBottom="0px"
-            fontFamily="Clash Display"
-            fontWeight="600"
-            fontSize="200px"
-            lineHeight="200px"
-            letterSpacing="0.25px"
-            color="#FFFFFF"
-            marginRight="32px"
-          >
-            Drip
-          </Chakra.Text>
-          <Chakra.Box maxWidth="507px">
+const Cta = () => {
+  return (
+    <Chakra.Box position="relative" mb="-28px" width="100%">
+      <Chakra.Image width="100%" src={CtaImage}></Chakra.Image>
+      <Chakra.Box width="100%" position="absolute" top="0px" bottom="0px">
+        <Chakra.Box
+          display="flex"
+          alignItems="center"
+          px="60px"
+          maxWidth="1394px"
+          mx="auto"
+        >
+          <Chakra.Box paddingTop="56px" alignItems="center" display="flex">
             <Chakra.Text
               marginBottom="0px"
               fontFamily="Clash Display"
-              fontWeight="500"
-              fontSize="24px"
-              lineHeight="30px"
-              letterSpacing="0.20px"
+              fontWeight="600"
+              fontSize="200px"
+              lineHeight="200px"
+              letterSpacing="0.25px"
               color="#FFFFFF"
+              marginRight="32px"
             >
-              Get your drip apparel at a discount this festive season. We’ve
-              been selling all year and if you haven’t bought yet, you might as
-              well take this opportunity to buy.
+              Drip
             </Chakra.Text>
+            <Chakra.Box maxWidth="507px">
+              <Chakra.Text
+                marginBottom="0px"
+                fontFamily="Clash Display"
+                fontWeight="500"
+                fontSize="24px"
+                lineHeight="30px"
+                letterSpacing="0.20px"
+                color="#FFFFFF"
+              >
+                Get your drip apparel at a discount this festive season. We’ve
+                been selling all year and if you haven’t bought yet, you might
+                as well take this opportunity to buy.
+              </Chakra.Text>
+            </Chakra.Box>
           </Chakra.Box>
         </Chakra.Box>
       </Chakra.Box>
-    </Chakra.Box>
-    <Chakra.Box
-      display="flex"
-      alignItems="center"
-      width="100%"
-      bottom="56px"
-      position="absolute"
-    >
       <Chakra.Box
         display="flex"
         alignItems="center"
-        justifyContent="flex-start"
-        px="60px"
-        maxWidth="1394px"
         width="100%"
-        mx="auto"
+        bottom="56px"
+        position="absolute"
       >
-        <Chakra.Text
-          marginBottom="0px"
-          fontFamily="Clash Display"
-          fontWeight="300"
-          fontSize={{ base: "16px", xl: "24px" }}
-          lineHeight={{ base: "16px", xl: "50px" }}
-          marginRight="27px"
-          marginLeft="26px"
+        <Chakra.Box
+          display="flex"
+          alignItems="center"
+          justifyContent="flex-start"
+          px="60px"
+          maxWidth="1394px"
+          width="100%"
+          mx="auto"
         >
-          Shop the collection
-        </Chakra.Text>
-        <ArrowIcon height="28px" width="44px" />
+          <Chakra.Text
+            marginBottom="0px"
+            fontFamily="Clash Display"
+            fontWeight="300"
+            fontSize={{ base: "16px", xl: "24px" }}
+            lineHeight={{ base: "16px", xl: "50px" }}
+            marginRight="27px"
+            marginLeft="26px"
+          >
+            Shop the collection
+          </Chakra.Text>
+          <ArrowIcon height="28px" width="44px" />
+        </Chakra.Box>
+      </Chakra.Box>
+      <Chakra.Box position="absolute" bottom="0px" right="0px">
+        <CtaSvg />
       </Chakra.Box>
     </Chakra.Box>
-    <Chakra.Box position="absolute" bottom="0px" right="0px">
-      <CtaSvg />
-    </Chakra.Box>
-  </Chakra.Box>
-)
+  )
+}
 
 export default Cta
--- src/components/gallery.js
+++ src/components/gallery.js
@@ -5,64 +5,113 @@ import GalleryImageTwo from "../images/gallery-image-two.png"
 import GalleryImageThree from "../images/gallery-image-three.png"
 import GalleryImageFour from "../images/gallery-image-four.png"
 import ArrowIcon from "../images/collections-arrow.svg"
+import { gsap, Power2 } from "gsap"
 // import { Link } from "gatsby"
 
-const Gallery = ({ siteTitle }) => (
-  <Chakra.Box
-    mt="143px"
-    mb="157px"
-    position="relative"
-    backgroundColor="#FFFFFF"
-  >
+const Gallery = () => {
+  let FirstImage = React.useRef(null)
+  let SecondImage = React.useRef(null)
+  let ThirdImage = React.useRef(null)
+  let FourthImage = React.useRef(null)
+
+  React.useEffect(() => {
+    //    gsap.to(container, 0, { css: { visibility: "visible" } })
+    //    // console.log(imageReveal);
+    //    gsap.to(imageReveal, 1.8, {
+    //      css: { width: "0%" },
+    //      ease: Power2.easeInOut,
+    //    })
+    var tl = gsap.timeline({ repeat: -1, repeatDelay: 2 })
+    console.log(FirstImage)
+    tl.to(FirstImage, { x: 450, duration: 3, ease: Power2.easeInOut })
+    tl.to(SecondImage, { x: 450, duration: 3, ease: Power2.easeInOut }, "-=75%")
+    tl.to(ThirdImage, { x: -350, duration: 3, ease: Power2.easeInOut }, "-=75%")
+    tl.to(
+      FourthImage,
+      { x: -800, duration: 3, ease: Power2.easeInOut },
+      "-=75%"
+    )
+    tl.to(FirstImage, { x: 0, duration: 3, ease: Power2.easeInOut }, "+=20%")
+    tl.to(SecondImage, { x: 0, duration: 3, ease: Power2.easeInOut }, "-=75%")
+    tl.to(ThirdImage, { x: -20, duration: 3, ease: Power2.easeInOut }, "-=75%")
+    tl.to(FourthImage, { x: -70, duration: 3, ease: Power2.easeInOut }, "-=75%")
+    tl.to(ThirdImage, { x: 0, duration: 1, ease: Power2.easeInOut })
+    tl.to(FourthImage, { x: 0, duration: 1, ease: Power2.easeInOut }, "-=75%")
+    //    gsap.from(FirstImage, 1.4, {
+    //      scale: 1.8,
+    //      ease: Power2.easeInOut,
+    //      delay: -1.8,
+    //    })
+  })
+  return (
     <Chakra.Box
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      px="60px"
-      maxWidth="1394px"
-      mx="auto"
+      mt="143px"
+      mb="157px"
+      position="relative"
+      backgroundColor="#FFFFFF"
     >
-      <Chakra.Image src={GalleryImageOne}></Chakra.Image>
-      <Chakra.Image
-        mt="187px"
-        ml="-51px"
-        zIndex="2"
-        src={GalleryImageTwo}
-      ></Chakra.Image>
-      <Chakra.Image ml="-180px" src={GalleryImageThree}></Chakra.Image>
-      <Chakra.Image ml="10px" src={GalleryImageFour}></Chakra.Image>
       <Chakra.Box
         display="flex"
         alignItems="center"
-        width="100%"
-        bottom="0px"
-        position="absolute"
+        justifyContent="center"
+        px="60px"
+        maxWidth="1394px"
+        mx="auto"
       >
+        <Chakra.Image
+          ref={el => (FirstImage = el)}
+          src={GalleryImageOne}
+        ></Chakra.Image>
+        <Chakra.Image
+          ref={el => (SecondImage = el)}
+          mt="187px"
+          ml="-51px"
+          zIndex="2"
+          src={GalleryImageTwo}
+        ></Chakra.Image>
+        <Chakra.Image
+          ref={el => (ThirdImage = el)}
+          ml="-180px"
+          src={GalleryImageThree}
+        ></Chakra.Image>
+        <Chakra.Image
+          ref={el => (FourthImage = el)}
+          ml="10px"
+          src={GalleryImageFour}
+        ></Chakra.Image>
         <Chakra.Box
           display="flex"
           alignItems="center"
-          justifyContent="flex-start"
-          px="60px"
-          maxWidth="1394px"
           width="100%"
-          mx="auto"
+          bottom="20px"
+          position="absolute"
         >
-          <Chakra.Text
-            marginBottom="0px"
-            fontFamily="Clash Display"
-            fontWeight="500"
-            fontSize={{ base: "16px", xl: "24px" }}
-            lineHeight={{ base: "16px", xl: "50px" }}
-            marginRight="27px"
-            marginLeft="26px"
+          <Chakra.Box
+            display="flex"
+            alignItems="center"
+            justifyContent="flex-start"
+            px="60px"
+            maxWidth="1394px"
+            width="100%"
+            mx="auto"
           >
-            Shop the collection
-          </Chakra.Text>
-          <ArrowIcon height="28px" width="44px" />
+            <Chakra.Text
+              marginBottom="0px"
+              fontFamily="Clash Display"
+              fontWeight="500"
+              fontSize={{ base: "16px", xl: "24px" }}
+              lineHeight={{ base: "16px", xl: "50px" }}
+              marginRight="27px"
+              marginLeft="26px"
+            >
+              Shop the collection
+            </Chakra.Text>
+            <ArrowIcon height="28px" width="44px" />
+          </Chakra.Box>
         </Chakra.Box>
       </Chakra.Box>
     </Chakra.Box>
-  </Chakra.Box>
-)
+  )
+}
 
 export default Gallery
